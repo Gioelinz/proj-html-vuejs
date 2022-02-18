65682114530d7526208fa8e6d4b60b4875c4ac29
Gioelinz <gioelerunza.gr@gmail.com>
2022-02-18 11:30:47 +0100

    add all imgs

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,36 @@
 <template>
-  <header></header>
+  <header>
+    <div class="container">
+      <nav>
+        <ul class="nav">
+          <li v-for="(item, index) in nav" :key="index">
+            <a class="item-nav" href="#">{{ item.text }}</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
   name: "Header",
+  props: ["nav"],
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../assets/scss/_vars.scss";
+header {
+  background-color: white;
+  height: 100px;
+  .item-nav {
+    color: $gray;
+    text-decoration: none;
+    font-weight: 800;
+    font-size: 12px;
+    letter-spacing: 1px;
+    margin: 0 10px;
+  }
+}
 </style>
