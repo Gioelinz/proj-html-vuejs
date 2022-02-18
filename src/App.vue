e41ceae4d68b7844913fe6c444c9f1d2b7f88eed
Gioelinz <gioelerunza.gr@gmail.com>
2022-02-18 11:10:07 +0100

    add Header comp, e nav elements as array

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <!-- Sezione Header -->
+    <Header :nav="navElements" />
+  </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header,
+  },
+  data() {
+    return {
+      navElements: [
+        { text: "HOME" },
+        { text: "PAGES" },
+        { text: "PROGRAM" },
+        { text: "TICKETS" },
+        { text: "SPEAKERS" },
+        { text: "PAPERS" },
+        { text: "BLOG" },
+        { text: "SHORTCODES" },
+      ],
+    };
+  },
 };
 </script>
 
 <style lang="scss">
+@import "./assets/scss/generics.scss";
 body {
   font-family: Helvetica, sans-serif;
 }
